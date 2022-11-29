f3bc1971d803dfdda4ceb614ab4edc86a87c3437
Gleb921 <119343853+Gleb921@users.noreply.github.com>
2022-11-29 19:13:53 +0100

    Add files via upload

--- ConsoleApplication30.cpp
+++ ConsoleApplication30.cpp
@@ -1,20 +1,148 @@
-﻿// ConsoleApplication30.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
 
-#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void checkInput()
+{
+    if (cin.fail())
+    {
+        throw "Incorrect input";
+    }
+}
+
+void checkBounds(int a, int b)
+{
+    if (a >= b)
+    {
+        throw "Incorrect loop bounds input";
+    }
+}
+
+void checkUpperBound(int n)
+{
+    if (n < 4)
+    {
+        throw "Incorrect upper bound input";
+    }
+}
+
+void checkStep(int h)
+{
+    if (h <= 0)
+
+    {
+        throw "Incorrect step input";
+    }
+}
+
+void filePrint(double x, double y, int n)
+{
+    ofstream fout("test.txt");
+    fout << "iteration number  " ", x = " << x << ", y = " << y << "\n";
+
+}
+
+int calculate(double n, double x)
+
+{
+
+    double s = 1;
+
+    double y = 0;
+
+    int j, i;
+
+    if (x < 0)
+
+    {
+
+        for (j = 2;j <= n - 2;j++)
+
+        {
+
+            s *= j * j;
+
+        }
+
+    }
+
+
+    else if (x >= 0)
+
+    {
+        for (i = 0;i <= n - 1;i++)
+
+        {
+
+            for (j = 0;j <= n - 1;j++)
+
+            {
+
+                s *= x + i * i + j;
+
+            }
+
+            y += s;
+
+        }
+        return y;
+    }
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double a = 0;
+    double b = 0;
+    double h = 0;
+    int n = 0;
+
+    double y = 0;
+    double x = 0;
+
+    try
+    {
+        cout << "Enter a < b ";
+        cin >> a;
+        cout << "Enter b ";
+        cin >> b;
+        checkInput();
+        checkBounds(a, b);
+        cout << "Enter h > 0 ";
+        cin >> h;
+        checkInput();
+        checkStep(h);
+        cout << "Enter n > 2 ";
+        cin >> n;
+        checkInput();
+        checkUpperBound(n);
+
+        x = a;
+
+        while (x <= b)
+        {
+
+
+            y = calculate(x, n);
+            cout << "\'x\' and \'y\' values are: " << x << ", " << y << endl;
+
+            filePrint(x, y, n);
+
+            x += h;
+        }
+    }
+    catch (const char* ex)
+    {
+        cout << ex << endl;
+    }
+    catch (...)
+    {
+        cout << "Unknown error" << endl;
+        return -2;
+    }
+
+    return 0;
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
--- UnitTest1.cpp
+++ UnitTest1.cpp
@@ -1,16 +1,136 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "C:\Users\Gleba\source\repos\ConsoleApplication27\ConsoleApplication27\ConsoleApplication27.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTest1
+namespace UnitTestS
 {
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(calculate_Tests)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(calculate_getnegative10and10_expected0)
+
 		{
+			double x = -10;
+			int n = 10;
+			double expected = 0;
+
+			double actual = calculate(x, n);
+			Assert::AreEqual(expected, actual);
 		}
 	};
+
+
+	TEST_CLASS(checkBounds_Tests)
+	{
+	public:
+		TEST_METHOD(checkBounds_get2and12_exceptionNotThrown)
+		{
+			int a = 2;
+			int b = 12;
+
+			try
+			{
+				checkBounds(a, b);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail;
+			}
+		}
+
+
+	public:
+		TEST_METHOD(checkBounds_get10and5_exceptionThrown)
+		{
+			int a = 10;
+			int b = 5;
+
+			try
+			{
+				checkBounds(a, b);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+
+	TEST_CLASS(checkUpperBound_Tests)
+	{
+	public:
+		TEST_METHOD(checkUpperBound_get5_exceptionNotThrown)
+		{
+			int n = 5;
+
+			try
+
+			{
+				checkUpperBound(n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+
+	public:
+		TEST_METHOD(checkUpperBound_get1_exceptionThrown)
+		{
+			int n = 1;
+
+			try
+			{
+				checkUpperBound(n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+
+	TEST_CLASS(checkStep_Tests)
+
+	{
+	public:
+		TEST_METHOD(checkStep_get3_exceptionNotThrown)
+		{
+			int h = 3;
+
+			try
+			{
+				checkStep(h);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+		}
+
+	public:
+		TEST_METHOD(checkStep_getnegative5_exceptionThrown)
+		{
+			int h = -5;
+
+			try
+			{
+				checkStep(h);
+				Assert::Fail();
+			}
+
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+
 }
